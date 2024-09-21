13e039e97255032f2c14e26294d25e4f2087d392
naburnm8 <alint@bk.ru>
2024-09-22 01:49:35 +0300

    paskhalko added

--- src/main/java/ru/naburnm8/bmstu/datamanagementnirbackend/controllers/UtilityController.java
+++ src/main/java/ru/naburnm8/bmstu/datamanagementnirbackend/controllers/UtilityController.java
@@ -11,11 +11,15 @@ import ru.naburnm8.bmstu.datamanagementnirbackend.models.Message;
 public class UtilityController {
     @GetMapping
     public ResponseEntity<Message> getConnection() {
-
         return ResponseEntity.ok(new Message("borovik"));
     }
     @GetMapping("/info")
     public ResponseEntity<Message> getInfo() {
-        return ResponseEntity.ok(new Message("PostgreSQL API Server running on Windows 11"));
+        return ResponseEntity.ok(new Message("Да я зашел, смотрел [REDACTED] на динамиках." +
+                " Она спросила, че это там у тебя. Я говорю [REDACTED]. Она спросила," +
+                " за [REDACTED] он или нет. Я говорю против. Она говорит, что такое смотреть не надо." +
+                " Я ответил, что он еврей, а еврей глупостей говорит не будет. Тут она начала разгонять," +
+                " что мою сестру за тебя надо выдавать замуж." +
+                " Я сказал, что не желаю тебе такой участи, а потом она выдала, что Катя будет дурой, если выйдет за меня, потому что я ирод, этакая гангрена и вредина"));
     }
 }
